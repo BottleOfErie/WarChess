bdca480847fa6723ae68d64fc1c89ce70df30524
0d00 <ncl5228800@163.com>
2023-11-29 22:34:09 +0800

    remove redundant designs

--- src/ck/ckrc/erie/warchess/Main.java
+++ src/ck/ckrc/erie/warchess/Main.java
@@ -1,12 +1,12 @@
 package ck.ckrc.erie.warchess;
 
-import ck.ckrc.erie.warchess.example.GunTower;
-import ck.ckrc.erie.warchess.example.Miner;
 import ck.ckrc.erie.warchess.game.ChessClassLoader;
 import ck.ckrc.erie.warchess.game.Engine;
 import ck.ckrc.erie.warchess.net.MapSyncThread;
 import ck.ckrc.erie.warchess.ui.CLIUserInterface;
 import ck.ckrc.erie.warchess.utils.Logger;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -29,8 +29,10 @@ public class Main {
 
         currentGameEngine=new Engine();
         chessClassLoader=new ChessClassLoader();
-
+        FXMain.FXMain(args);
         CLIUserInterface.cli_main();
 
     }
+
+
 }
